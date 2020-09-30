d808ed63f0085f4f495e29d6235ddab58cae26bc
Anton Ukhanev <xedin.unknown@gmail.com>
2020-09-30 14:02:23 +0200

    Back-ported features, docs, and configs from `wp-oop/plugin-boilerplate`
    PHPStorm configs created anew.

---
+++ tests/Func/ThingTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Me\Library\Test\Func;
+
+use Me\Library\Thing;
+use PHPUnit\Framework\TestCase;
+
+class ThingTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(false);
+    }
+}
